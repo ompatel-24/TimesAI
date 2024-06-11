917fe9a86368a693aeb39197e24aa5ee7667feaf
Om Patel <ompatel801@icloud.com>
2024-06-11 17:27:18 -0400

    push

---
+++ TimesAI/StatisticsView.swift
@@ -0,0 +1,18 @@
+//
+//  StatisticsView.swift
+//  TimesAI
+//
+//  Created by Om Patel on 2024-06-10.
+//
+
+import SwiftUI
+
+struct StatisticsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StatisticsView()
+}
